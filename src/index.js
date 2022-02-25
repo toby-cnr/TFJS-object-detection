a05d78bb8a7c795f583c36a1270977993905d40a
Toby Lam <92720380+toby-cnr@users.noreply.github.com>
2022-02-25 17:21:43 +0000

    Updated for shape model

--- src/index.js
+++ src/index.js
@@ -5,24 +5,36 @@ import {loadGraphModel} from '@tensorflow/tfjs-converter';
 import "./styles.css";
 tf.setBackend('webgl');
 
-const threshold = 0.75;
+const threshold = 0.5;
 
 async function load_model() {
     // It's possible to load the model locally or from a repo
     // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
     //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
+    const model = await loadGraphModel("https://github.com/toby-cnr/TFJS-object-detection/raw/master/models/pdf-shape-detector/model.json");
     return model;
   }
 
 let classesDir = {
     1: {
-        name: 'Kangaroo',
+        name: '3d',
         id: 1,
     },
     2: {
-        name: 'Other',
+        name: 'front',
         id: 2,
+    },
+    2: {
+        name: 'info',
+        id: 3,
+    },
+    2: {
+        name: 'side',
+        id: 4,
+    },
+    2: {
+        name: 'top',
+        id: 5,
     }
 }
 
@@ -152,8 +164,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Real-Time Object Detection: Kangaroo</h1>
-        <h3>MobileNetV2</h3>
+        <h1>Real-Time Object Detection Demo</h1>
+        <h3>Created by Toby</h3>
         <video
           style={{height: '600px', width: "500px"}}
           className="size"
